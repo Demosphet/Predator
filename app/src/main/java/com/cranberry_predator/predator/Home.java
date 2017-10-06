49e0cc131cb15b18472301eaa1726cdeb72a74e0
Demosphet <sachsiri@live.co.uk>
2017-10-06 18:48:02 +1100

    Login Screen of the Predator App and Established Connection to the MySQL Database

--- app/src/main/java/com/cranberry_predator/predator/Home.java
+++ app/src/main/java/com/cranberry_predator/predator/Home.java
@@ -1,13 +1,30 @@
 package com.cranberry_predator.predator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class Home extends AppCompatActivity {
+
+    EditText usernameEditText;
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+    }
+
+    public void OnLogin(View view){
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String type = "login";
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
     }
 }
